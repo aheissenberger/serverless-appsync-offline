fa18e31f5976c5337d5e492cc30269196c407d15
Andreas Heissenberger <andreas@heissenberger.at>
2018-09-12 06:05:53 +0200

    first BETA Version

---
+++ README.md
@@ -0,0 +1,130 @@
+serverless-appsync-offline
+=================================
+
+[![npm version](https://badge.fury.io/js/serverless-appsync-offline.svg)](https://badge.fury.io/js/serverless-appsync-offline)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## This Plugin Requires
+* serverless@v1-rc.1
+* Java Runtime Engine (JRE) version 6.x or newer
+
+## Features
+* Emulate Appsync with [AppSync Emulator](https://github.com/ConduitVC/aws-utils/tree/appsync/packages/appsync-emulator-serverless) and depends on [Serverless-AppSync-Plugin](https://github.com/sid88in/serverless-appsync-plugin)
+* Connect to any DynamoDB or install DynamoDB Local
+* Start DynamoDB Local with all the parameters supported (e.g port, inMemory, sharedDb)
+* Table Creation for DynamoDB Local
+
+## Install Plugin
+`npm install --save serverless-appsync-offline`
+
+Then in `serverless.yml` add following entry to the plugins array: `serverless-appsync-offline`
+```yml
+plugins:
+  - serverless-appsync-offline
+```
+
+## Using the Plugin
+
+
+1) Add Appsync Resource definitions to your Serverless configuration, as defined here: https://github.com/sid88in/serverless-appsync-plugin#configuring-the-plugin
+
+
+
+
+## Start: sls appsync-offline start
+All CLI options are optional:
+
+```
+--port  		  -p  Port to listen on. Default: 8000
+--inMemory                -i  DynamoDB; will run in memory, instead of using a database file. When you stop DynamoDB;, none of the data will be saved. Note that you cannot specify both -dbPath and -inMemory at once.
+--dbPath                  -d  The directory where DynamoDB will write its database file. If you do not specify this option, the file will be written to the current directory. Note that you cannot specify both -dbPath and -inMemory at once. For the path, current working directory is <projectroot>/node_modules/serverless-appsync-offline/dynamob. For example to create <projectroot>/node_modules/serverless-appsync-offline/dynamob/<mypath> you should specify -d <mypath>/ or --dbPath <mypath>/ with a forwardslash at the end.
+--sharedDb                -h  DynamoDB will use a single database file, instead of using separate files for each credential and region. If you specify -sharedDb, all DynamoDB clients will interact with the same set of tables regardless of their region and credential configuration.
+--delayTransientStatuses  -t  Causes DynamoDB to introduce delays for certain operations. DynamoDB can perform some tasks almost instantaneously, such as create/update/delete operations on tables and indexes; however, the actual DynamoDB service requires more time for these tasks. Setting this parameter helps DynamoDB simulate the behavior of the Amazon DynamoDB web service more closely. (Currently, this parameter introduces delays only for global secondary indexes that are in either CREATING or DELETING status.)
+--optimizeDbBeforeStartup -o  Optimizes the underlying database tables before starting up DynamoDB on your computer. You must also specify -dbPath when you use this parameter.
+```
+
+All the above options can be added to serverless.yml to set default configuration: e.g.
+
+**Minimum Options:**
+```yml
+appsync-offline:
+    port: 62222
+    dynamodb:
+      server:
+        port: 8000
+```
+**All Options:**
+```yml
+appsync-offline:
+    port: 62222
+    dynamodb:
+      client:
+        # if endpoint is provided, no local database server is started and and appsync connects to the endpoint - e.g. serverless-dynamodb-local
+        endpoint: "http://localhost:8000"
+        region: localhost
+        accessKeyId: a
+        secretAccessKey: a
+      server:
+        port: 8000
+        dbPath: "./.dynamodb"
+        inMemory: false,
+        sharedDb: false,
+        delayTransientStatuses: false,
+        optimizeDbBeforeStartup: false,
+```
+
+
+## Using DynamoDB Local in your code
+You need to add the following parameters to the AWS NODE SDK dynamodb constructor
+
+e.g. for dynamodb document client sdk
+```
+var AWS = require('aws-sdk');
+```
+```
+new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+})
+```
+e.g. for dynamodb document client sdk
+```
+new AWS.DynamoDB({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+})
+```
+
+### Using with serverless-offline plugin
+When using this plugin with serverless-offline, it is difficult to use above syntax since the code should use DynamoDB Local for development, and use DynamoDB Online after provisioning in AWS. Therefore we suggest you to use [serverless-dynamodb-client](https://github.com/99xt/serverless-dynamodb-client) plugin in your code.
+
+The `serverless appsync-offline start` command can be triggered automatically when using `serverless-offline` plugin.
+
+
+Add both plugins to your `serverless.yml` file:
+```yaml
+plugins:
+  - serverless-appsync-offline
+  - serverless-offline
+```
+
+Make sure that `serverless-appsync-offline` is above `serverless-offline` so it will be loaded earlier.
+
+Now your local Appsync and the DynamoDB database will be automatically started before running `serverless offline`.
+
+### Using with serverless-offline and serverless-webpack plugin
+Run `serverless offline start`. In comparison with `serverless offline`, the `start` command will fire an `init` and a `end` lifecycle hook which is needed for serverless-offline and serverless-appsync-offline to switch off both ressources. 
+
+Add plugins to your `serverless.yml` file:
+```yaml
+plugins:
+  - serverless-webpack
+  - serverless-appsync-offline
+  - serverless-offline #serverless-offline needs to be last in the list
+```
+## Notes
+
+The [AppSync Emulator](https://github.com/ConduitVC/aws-utils/tree/appsync/packages/appsync-emulator-serverless) does not support CloudFormation syntax (e.g. `tableName: { Ref: UsersTable }`) in `dataSources`. 
+
+## License
+  [MIT](LICENSE)
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 const _ = require("lodash");
+const path = require("path");
 const dynamoEmulator = require("@conduitvc/dynamodb-emulator");
 const createServer = require("@conduitvc/appsync-emulator-serverless/server");
 
@@ -7,19 +8,15 @@ class ServerlessAppSyncPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.serverlessLog = serverless.cli.log.bind(serverless.cli);
-    this.options = _.merge(
-      {
-        localPath: path.join(serverless.config.servicePath, ".dynamodb")
-      },
-      options
-    );
+    this.options = options;
 
     this.commands = {
       "appsync-offline": {
+        usage: "Simulates local AppSync",
         commands: {
           start: {
             lifecycleEvents: ["startHandler"],
-            usage: "Starts local DynamoDB",
+            usage: "Simulates local AppSync",
             options: {
               port: {
                 shortcut: "p",
@@ -30,6 +27,31 @@ class ServerlessAppSyncPlugin {
                 shortcut: "d",
                 usage:
                   "The port number that DynamoDB will use to communicate with your application. If you do not specify this option, the default port is dynamic"
+              },
+              inMemory: {
+                shortcut: "i",
+                usage:
+                  "DynamoDB; will run in memory, instead of using a database file. When you stop DynamoDB;, none of the data will be saved. Note that you cannot specify both -dbPath and -inMemory at once."
+              },
+              dbPath: {
+                shortcut: "b",
+                usage:
+                  "The directory where DynamoDB will write its database file. If you do not specify this option, the file will be written to the current directory. Note that you cannot specify both -dbPath and -inMemory at once. For the path, current working directory is <projectroot>/node_modules/serverless-dynamodb-local/dynamob. For example to create <projectroot>/node_modules/serverless-dynamodb-local/dynamob/<mypath> you should specify -d <mypath>/ or --dbPath <mypath>/ with a forwardslash at the end."
+              },
+              sharedDb: {
+                shortcut: "h",
+                usage:
+                  "DynamoDB will use a single database file, instead of using separate files for each credential and region. If you specify -sharedDb, all DynamoDB clients will interact with the same set of tables regardless of their region and credential configuration."
+              },
+              delayTransientStatuses: {
+                shortcut: "t",
+                usage:
+                  "Causes DynamoDB to introduce delays for certain operations. DynamoDB can perform some tasks almost instantaneously, such as create/update/delete operations on tables and indexes; however, the actual DynamoDB service requires more time for these tasks. Setting this parameter helps DynamoDB simulate the behavior of the Amazon DynamoDB web service more closely. (Currently, this parameter introduces delays only for global secondary indexes that are in either CREATING or DELETING status."
+              },
+              optimizeDbBeforeStartup: {
+                shortcut: "o",
+                usage:
+                  "Optimizes the underlying database tables before starting up DynamoDB on your computer. You must also specify -dbPath when you use this parameter."
               }
             }
           }
@@ -51,26 +73,36 @@ class ServerlessAppSyncPlugin {
   }
 
   startHandler() {
+    this._setOptions();
+    var self = this;
     (async function() {
-      // start the dynamodb emulator
-      this.serverlessLog('start dynamodb');
-      this.emulator = await dynamoEmulator.launch({
-        dbPath: this.options.localPath,
-        port: 8000
-      });
-
-      const dynamodb = dynamoEmulator.getClient(emulator);
-
+      let dynamodb = null;
+      if (self.options.dynamodb.client.endpoint) {
+        const { DynamoDB } = require("aws-sdk");
+        console.log("options", options);
+        dynamodb = new DynamoDB(self.options.dynamodb.client);
+      } else {
+        // start the dynamodb emulator
+        self.emulator = await dynamoEmulator.launch(
+          self.options.dynamodb.server
+        );
+        dynamodb = dynamoEmulator.getClient(
+          self.emulator,
+          self.options.dynamodb.client
+        );
+        self.serverlessLog("dynamodb started: " + dynamodb.endpoint.href);
+        //self.serverlessLog(JSON.stringify( dynamodb))
+      }
       const serverless = path.join(
-        serverless.config.servicePath,
+        self.serverless.config.servicePath,
         "serverless.yml"
       );
-      const port = 62222;
+      const port = self.options.port;
       const server = await createServer({ serverless, port, dynamodb });
-      this.serverlessLog('appsync started: ' + server.url);
+      self.serverlessLog("appsync started: " + server.url);
     })().then(
-      _ => this.serverlessLog('OK'),
-      err => this.serverlessLog("ERROR: " + err)
+      _ => self.serverlessLog("OK"),
+      err => self.serverlessLog("ERROR: " + err)
     );
   }
   endHandler() {
@@ -78,6 +110,45 @@ class ServerlessAppSyncPlugin {
     this.emulator.terminate().then(() => {
       process.exit(0);
     });
+  }
+
+  _setOptions() {
+    // Merge the different sources of values for this.options
+    // Precedence is: command line options, YAML options, defaults.
+    //this.serverlessLog(JSON.stringify(this.options));
+    const defaultOpts = {
+      port: null,
+      dynamodb: {
+        client: {
+          region: "localhost",
+          accessKeyId: "MOCK_ACCESS_KEY_ID",
+          secretAccessKey: "MOCK_SECRET_ACCESS_KEY",
+        },
+        server: {
+          port: null,
+          dbPath: path.join(this.serverless.config.servicePath, ".dynamodb"),
+          inMemory: false
+        }
+      }
+    };
+    this.options = _.merge(
+      {},
+      defaultOpts,
+      (this.serverless.service.custom || {})["appsync-offline"],
+      {
+        port: this.options.port,
+        dynamodb: {
+          server: {
+            port: this.options.dynamoDbPort,
+            dbPath: this.options.dbPath,
+            inMemory: this.options.inMemory,
+            sharedDb: this.options.sharedDb,
+            delayTransientStatuses: this.options.delayTransientStatuses,
+            optimizeDbBeforeStartup: this.options.optimizeDbBeforeStartup
+          }
+        }
+      }
+    );
   }
 }
 
